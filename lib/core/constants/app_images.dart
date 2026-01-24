d73754f7cba68180c3e3f2a20d9549746c00ab2e
3bowhab <ali.abdelwhab21@gmail.com>
2026-01-24 16:45:20 +0200

    first commit

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for svgBeingCreative
+  /// assets/svg/being_creative.svg
+  static const String svgBeingCreative = "assets/svg/being_creative.svg";
+}
+
